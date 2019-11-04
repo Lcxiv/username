b31ed310ee5458cc4dab62eaf01d2d2ad119feec
Louis Condevaux <condevaux@chapman.edu>
2019-11-04 10:44:45 -0800

    Working username

--- username.sh
+++ username.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 # username.sh
 # Louis Condevaux
-echo "It must only contains lower case letters, digits, and an underscore. It also must start with a lower letter and contain between three and twelve characters): "
-read ZIP
-while echo "$ZIP" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+echo "Your username  must only contains lower case letters, digits, and an underscore. It also must start with a lower letter and contain between three and twelve characters): "
+read USERNAME
+while echo "$USERNAME" | egrep -v "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
-	echo "Not a lower case as first character."
-	echo "Not enough characters"
-	read ZIP
+	
+	echo "Wrong input."
+	echo "Enter another username:"
+	read USERNAME
 done
 echo "Thank you"
